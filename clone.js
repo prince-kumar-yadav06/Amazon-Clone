39a3fe83b9a237ad37dc730c4ef3b3eac1f122f6
prince-kumar-yadav06 <163705172+prince-kumar-yadav06@users.noreply.github.com>
2024-04-24 16:15:00 +0530

    Add files via upload
    This project is created by using Html CSS only.

---
+++ clone.js
@@ -0,0 +1,22 @@
+
+let mybutton = document.getElementById("myBtn");
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "bolck";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+const lastbottom=document.querySelector('.last-botton');
+const list=document.querySelector(".list");
+
+maindrop.addEventListener('click', ()=>{
+  list.classList.toggle('show');
+})
